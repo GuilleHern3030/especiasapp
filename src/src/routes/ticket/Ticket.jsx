cd8067c0bba02d874aeb6f2fe920c3f4257bc023
GuilleHern3030 <hernandezguillermo1995@gmail.com>
2025-11-11 09:21:27 -0300

    Update ShoppingCart article deleter

--- src/src/routes/ticket/Ticket.jsx
+++ src/src/routes/ticket/Ticket.jsx
@@ -12,10 +12,11 @@ import Dialog from '../../components/dialog/Dialog'
 import styles from './Ticket.module.css'
 import logo from '../../assets/images/logo.png'
 import logo2 from '../../assets/icons/logo.png'
+import removeIcon from '../../assets/icons/delete.webp'
 
 import { credits, priceundefined } from '../../data/references.json'
 import { title } from '../../data/client-info.json'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const total = (cells) => {
     try {
@@ -40,11 +41,21 @@ const total = (cells) => {
 export default function Ticket() {
 
     const [ dialog, setDialog ] = useState()
+    const [ items, setItems ] = useState()
+    const cells = useCells()
+
+    const hasParams = () => cells?.params?.length > 0
+
+    useEffect(() => { loadItems() }, [cells])
+    useEffect(() => { console.log(items) }, [items])
+
+    const loadItems = () => {
+        if (Array.isArray(cells?.items))
+            setItems(cells.items.slice())
+    }
 
     const data = useData()
     
-    const cells = useCells()
-    console.log(cells)
 
     const navigate = useNavigate()
 
@@ -55,7 +66,10 @@ export default function Ticket() {
         window.open(data.contactlink + "?text=Hola, quiero consultar por este producto: \n\r" + link, "_blank")
     }
 
-    const handleBack = () => navigate('/', { replace:true })
+    const handleBack = () => {
+        navigate('/', { replace:true })
+        window.location.reload()
+    }
 
     const handleEmptyCart = () => {
         setDialog(<Dialog
@@ -65,7 +79,20 @@ export default function Ticket() {
                 sessionStorage.clear()
                 setDialog(null)
                 handleBack()
-                window.location.reload()
+            }}
+        />)
+    }
+
+    const handleDelete = (item, index) => {
+        setDialog(<Dialog
+            title={"¿Querés sacar este artículo de tu carrito?"}
+            message={item.name}
+            onReject={() => setDialog(null)}
+            onAccept={() => {
+                setDialog(null)
+                cells.items.splice(index, 1)
+                sessionStorage.removeItem("ITEM:" + cells.keys[index])
+                loadItems()
             }}
         />)
     }
@@ -86,33 +113,44 @@ export default function Ticket() {
             <img src={logo}/>
         </div>
 
-        <div className={styles.ticket}>
-            { cells ? <>
-                <div className={styles.title}> <p>Ticket</p> </div>
-                <hr/>
-                <div className={styles.items}>{
-                    cells.items.map((item, index) => <div key={index} >
-                            <p 
-                                id={cells.keys[index]}
-                                className={styles.item}
-                            >{`${item.name} (${item.header}): ${typeof(item.price) === 'string' ? item.price : priceundefined}`}</p>
-                        </div>
-                    )
-                }</div>
-                {
-                    total(cells)
+        { items?.length > 0 &&
+            <div className={styles.ticket}>
+                { cells ? <>
+                    <div className={styles.title}> <p>Ticket</p> </div>
+                    <hr/>
+                    <div className={styles.items}>{ items &&
+                        items.map((item, index) => <div key={index} className={styles.item}>
+                                { !hasParams() && <img onClick={() => handleDelete(item, index)} src={removeIcon}/> }
+                                <p 
+                                    id={cells.keys[index]}
+                                    
+                                >{`${item.name} (${item.header}): ${typeof(item.price) === 'string' ? item.price : priceundefined}`}</p>
+                            </div>
+                        )
+                    }</div>
+                    {
+                        total(cells)
+                    }
+                </> : <></>
                 }
-            </> : <></>
-            }
-        </div>
+            </div>
+        }
 
-        <div className={styles.button}>
-            <button onClick={handleQuery}>Consultar</button>
-        </div>
+        { (items?.length == 0 || !items) &&
+            <p className={styles.empty}>Aún no hay artículos en tu carrito</p>
+        }
 
-        <div className={styles.button} style={{display: cells?.params?.length > 0 ? 'none' : 'grid'}}>
-            <button onClick={handleEmptyCart}>Vaciar carrito</button>
-        </div>
+        { items?.length > 0 &&
+            <div className={styles.button}>
+                <button onClick={handleQuery}>Consultar</button>
+            </div>
+        }
+
+        { !hasParams() && items?.length > 0 && 
+            <div className={styles.button}>
+                <button onClick={handleEmptyCart}>Vaciar carrito</button>
+            </div> 
+        }
 
         <div className={styles.button}>
             <button onClick={handleBack}>Volver</button>
